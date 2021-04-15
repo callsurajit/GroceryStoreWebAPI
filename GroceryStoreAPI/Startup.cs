e52d8df7ac856e2b03ee0491527f355af4fbf610
Surajit Paul <surajit.paul@everbridge.com>
2021-04-15 14:32:31 -0400

    Swagger implemented to have an interactive API documentation

--- GroceryStoreAPI/Startup.cs
+++ GroceryStoreAPI/Startup.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
 
 namespace GroceryStoreAPI
 {
@@ -19,6 +21,21 @@ namespace GroceryStoreAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Grocery Store API",
+                    Description = "Coding skill evaluation process",
+                    TermsOfService = new Uri("https://example.com/terms"),                    
+                    License = new OpenApiLicense
+                    {
+                        Name = "Use under LICX",
+                        Url = new Uri("https://example.com/license"),
+                    }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -29,10 +46,16 @@ namespace GroceryStoreAPI
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Grocery Store v1");
+                //c.RoutePrefix = string.Empty;
+            });
+
             app.UseRouting();
-
             app.UseAuthorization();
-
+            
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
